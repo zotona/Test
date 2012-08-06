ff961bbfae387e92330670161d4087d0ccd42063
Ron <ronaldhatcher@mac.com>
2012-08-06 13:39:15 +0000

    Fixed up for 11.1.1.6 clipse install

--- deployOSB.cmd
+++ deployOSB.cmd
@@ -1,19 +1,19 @@
 @echo Export project from eclipse
 java ^
-  -Dweblogic.home=c:\oracle\middleware\wlserver_10.3 ^
-  -Dosb.home=c:\oracle\middleware\Oracle_OSB1 ^
+  -Dweblogic.home=D:\oracle\middleware\wlserver_10.3 ^
+  -Dosb.home=D:\oracle\middleware\Oracle_OSB1 ^
   -Dosgi.bundlefile.limit=500 ^
-  -Dharvester.home=c:\oracle\middleware\Oracle_OSB1\harvester ^
+  -Dharvester.home=D:\oracle\middleware\Oracle_OSB1\harvester ^
   -Dosgi.nl=en_US ^
   -Dsun.lang.ClassLoader.allowArraySyntax=true ^
-  -jar c:\Oracle\OEPE\oepe_11gR1PS4\plugins\org.eclipse.equinox.launcher_1.1.1.R36x_v20101122_1400.jar ^
-  -data c:\src\osb\notsvn ^
+  -jar D:\Oracle\Middleware\oepe_11.1.1.8.0\plugins\org.eclipse.equinox.launcher_1.2.0.v20110502.jar^
+  -data D:\src\osb\notsvn ^
   -application com.bea.alsb.core.ConfigExport ^
   -configProject "OSB Configuration 1" ^
-  -configJar c:\src\osb\test_sbconfig.jar ^
+  -configJar D:\src\osb\test_sbconfig.jar ^
   -configSubProjects "OSB Project 1" ^
   -includeDependencies true
 
 @echo Deploy 'import' to Service Bus
-c:\oracle\middleware\oracle_common\common\bin\wlst import.py import.properties
+D:\oracle\middleware\oracle_common\common\bin\wlst import.py import.properties
 
